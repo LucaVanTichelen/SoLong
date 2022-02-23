9af70bf12b8370b89bd9cd00c57de7867e215b72
Luca Van tichelen <luca_van_tic@hotmail.com>
2022-02-23 15:32:06 +0100

    Removed unnecessary create image functions

--- so_long.c
+++ so_long.c
@@ -6,11 +6,23 @@
 /*   By: lvan-tic <lvan-tic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 12:59:16 by lvan-tic          #+#    #+#             */
-/*   Updated: 2022/02/23 11:15:48 by lvan-tic         ###   ########.fr       */
+/*   Updated: 2022/02/23 14:43:36 by lvan-tic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	key_hook(int keycode)
+{
+	printf("%i\n", keycode);
+	return (0);
+}
+
+int	clean_exit()
+{
+	exit(0);
+	return (0);
+}
 
 int	main(void)
 {
@@ -29,21 +41,14 @@ int	main(void)
 	t_data	pirate;
 
 	fd = open("maps/map2.ber", O_RDONLY);
-	width = 32;
-	height = 32;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 34 * 32, 6 * 32, "so_long");
-	grass.img = mlx_new_image(mlx, 32, 32);
-	wall.img = mlx_new_image(mlx, 32, 32);
-	key.img = mlx_new_image(mlx, 32, 32);
-	chest.img = mlx_new_image(mlx, 32, 32);
-	pirate.img = mlx_new_image(mlx, 32, 32);
 	grass.img = mlx_xpm_file_to_image(mlx, "img/grass.xpm", &width, &height);
 	wall.img = mlx_xpm_file_to_image(mlx, "img/wall.xpm", &width, &height);
 	key.img = mlx_xpm_file_to_image(mlx, "img/key.xpm", &width, &height);
 	chest.img = mlx_xpm_file_to_image(mlx, "img/chest.xpm", &width, &height);
 	pirate.img = mlx_xpm_file_to_image(mlx, "img/pirate.xpm", &width, &height);
-
+	printf("%i - %i\n", width, height);
 	i = 0;
 	while ((str = get_next_line(fd)) != NULL)
 	{
@@ -74,5 +79,7 @@ int	main(void)
 		}
 		i++;
 	}
+	mlx_key_hook(mlx_win, key_hook, 0);
+	mlx_hook(mlx_win, 17, 0, clean_exit, 0);
 	mlx_loop(mlx);
 }
